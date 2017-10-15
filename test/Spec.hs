a3d049d8429e6e721263aaa75e4f4d293b08870c
Alexander Thiemann <mail@athiemann.net>
2017-10-14 20:05:53 -0700

    run the real ruby parser alongside

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Control.Monad
 import Data.Either
 import System.Directory
 import System.FilePath
+import System.Process
 import Test.Hspec
 import qualified Data.Text.IO as T
 
@@ -17,5 +18,9 @@ main =
                   getDirectoryContents "test-data/parser/passing"
               forM_ rbFiles $ \f ->
                   it ("Parses " ++ show f) $
-                  do ct <- T.readFile ("test-data/parser/passing" </> f)
+                  do let fullPath = "test-data/parser/passing" </> f
+                     -- check that the ruby implementation can parse the file
+                     callCommand $ "ruby -c " ++ fullPath
+                     -- check that our parser can parse it
+                     ct <- T.readFile fullPath
                      shouldSatisfy (execParser f parseProgram ct) isRight
